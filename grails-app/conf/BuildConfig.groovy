a8e2dc7ed0eb00662279c4632f4680c04db39fff
Thando Mafela <thandomafela@hotmail.com>
2012-01-11 20:49:34 +0000

    Added geb and spock to the application

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,31 +1,51 @@
-grails.project.class.dir = "target/classes"
-grails.project.test.class.dir = "target/test-classes"
-grails.project.test.reports.dir = "target/test-reports"
-//grails.project.war.file = "target/${appName}-${appVersion}.war"
+grails.project.work.dir = "target"
+
 grails.project.dependency.resolution = {
-    // inherit Grails' default dependencies
-    inherits("global") {
-        // uncomment to disable ehcache
-        // excludes 'ehcache'
-    }
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-    repositories {
-        grailsPlugins()
-        grailsHome()
-        grailsCentral()
+	inherits("global")
+	log "warn"
+	
+	def gebVersion = "0.6.0"
+	def seleniumVersion = "2.0rc3"
+	
+	repositories {
+		grailsPlugins()
+		grailsHome()
+		grailsCentral()
+		mavenLocal()
+		mavenCentral()
+		
+		// For Geb snapshot
+		mavenRepo "https://nexus.codehaus.org/content/repositories/snapshots"
+	}
+	dependencies {
+		test("org.seleniumhq.selenium:selenium-htmlunit-driver:$seleniumVersion") {
+			exclude "xml-apis"
+		}
+		test("org.seleniumhq.selenium:selenium-chrome-driver:$seleniumVersion")
+		test("org.seleniumhq.selenium:selenium-firefox-driver:$seleniumVersion")
 
-        // uncomment the below to enable remote dependency resolution
-        // from public Maven repositories
-        //mavenLocal()
-        //mavenCentral()
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
-    }
-    dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
+		// You usually only need one of these, but this project uses both
+		test "org.codehaus.geb:geb-spock:$gebVersion"
+		test "org.codehaus.geb:geb-junit4:$gebVersion"
 
-        // runtime 'mysql:mysql-connector-java:5.1.13'
-    }
+        runtime 'mysql:mysql-connector-java:5.1.13'
+	}
+	plugins {
+	    test ":tomcat:$grailsVersion"
+            test ":hibernate:$grailsVersion"
+		
+	    test ":geb:$gebVersion"
+	    test ":spock:0.5-groovy-1.7"
+        
+            compile ":joda-time:1.2"
+            compile ":jquery:1.6.1.1"
+            compile ":jquery-ui:1.8.11"
+            compile ":resources:1.1.3"
+            compile ":zipped-resources:1.0"
+
+
+            runtime ":build-test-data:1.1.1"
+            runtime ":fixtures:1.0.1"
+        
+	}
 }
