71f9621ecdbcc67c83cfe9cb6223e517c9e6a30d
thando <thandomafela@hotmail.com>
2012-02-19 19:59:39 +0000

    removed the stuff in pagecontroller

--- grails-app/controllers/com/gwayiit/HomePageController.groovy
+++ grails-app/controllers/com/gwayiit/HomePageController.groovy
@@ -3,29 +3,20 @@ package com.gwayiit
 class HomePageController {
 
     def index = {
-        def pagePart = PagePart.findByName('home')
-        [ pp: pagePart ]
+
     }
 
     def contact = {
-        def pagePart = PagePart.findByName('contact')
-        [ pp: pagePart ]
     }
 
     def team = {
-        def pagePart = PagePart.findByName('team')
-        [ pp: pagePart ]
     }
 
     def products = {
-        def pagePart = PagePart.findByName('products')
-        [ pp: pagePart ]
     }
 
     def recentNews = {}
 
     def aboutUs = {
-        def pagePart = PagePart.findByName('aboutUs')
-        [ pp: pagePart ]
     }
 }
